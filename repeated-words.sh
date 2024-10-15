bc3997b172b5ca015b500df4fe52e19431313e01
chandrika <chandrikaaws55@gmail.com>
2024-10-15 19:00:37 +0530

    aaa

--- repeated-words.sh
+++ repeated-words.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-#bash repeated-words.sh | sort | uniq -c | head -5
+#bash repeated-words.sh | sort | uniq -c | sort -nr | head -5
+
+
+#sort: Sorts the words alphabetically.
+#uniq -c: Counts the occurrences of each word.
+#sort -nr: Sorts the counts in numerical order, highest first.
+#head -5: Displays the top 5 results.
      
-#output is     13         API
-#              4         Business
-#              4         Data
-#              1         Professional
-#              1         Science
+#output is      13 API
+#               4 Data
+#               4 Business
+#               3 hello
+#               2 world
+
 
 echo "hello
 world 
