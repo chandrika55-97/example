03f29ec171a837b80824b8dbd3e8a4775cdabc78
chandrika <chandrikaaws55@gmail.com>
2024-10-15 19:29:26 +0530

    aa

--- row-colums.sh
+++ row-colums.sh
@@ -1,23 +1,6 @@
 #!/bin/bash
 
-# Convert Rows to Columns
-echo "Converting Rows to Columns:"
-awk '
-{
-    for (i = 1; i <= NF; i++) {
-        data[i, NR] = $i
-    }
-}
-END {
-    for (i = 1; i <= NF; i++) {
-        for (j = 1; j <= NR; j++) {
-            printf "%s ", data[i, j]
-        }
-        printf "\n"
-    }
-}' row-colums.sh
-
-echo -e "\nConverting Columns to Rows:"
-# Convert Columns to Rows
-awk '{for(i=1;i<=NF;i++) print $i}' row-colums.sh
+name age
+alice 21
+ryan 30
 
